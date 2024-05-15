53fbe8524fb9f0f9839d69115e360d42023b7fad
jeongmiri <1889543@naver.com>
2024-05-16 03:20:46 +0900

    feat: static 경로 설정

--- finni/django_project/urls.py
+++ finni/django_project/urls.py
@@ -18,14 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.static import serve
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("user/", include("apps.user.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -40,9 +36,11 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
-...
-
-urlpatterns += [
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("user/", include("apps.user.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path("swagger", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
